bd45ad83a6b24feaca39784015e71191ff9f874d
LepakRuslan <Lepak1987@gmail.com>
2020-10-17 10:17:57 -0500

    Tightly Coupled

---
+++ spring/Spring-00-TightlyCoupled/src/TestApp.java
@@ -0,0 +1,12 @@
+public class TestApp {
+
+    public static void main(String[] args) {
+        FullTimeMentor fullTimeMentor = new FullTimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
+
+        Mentor mentor = new Mentor(fullTimeMentor, partTimeMentor);
+
+        mentor.menageAccount();
+
+    }
+}
