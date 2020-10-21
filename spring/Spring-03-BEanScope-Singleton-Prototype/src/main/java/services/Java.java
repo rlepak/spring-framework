fd4c75b5783eac92bed93f9e072ce03753ecd4e8
LepakRuslan <Lepak1987@gmail.com>
2020-10-20 19:49:26 -0500

    Bean Scope

--- spring/Spring-03-BEanScope-Singleton-Prototype/src/main/java/services/Java.java
+++ spring/Spring-03-BEanScope-Singleton-Prototype/src/main/java/services/Java.java
@@ -2,18 +2,14 @@ package services;
 
 import interfaces.Course;
 import interfaces.ExtraSessions;
-import lombok.Data;
 import org.springframework.stereotype.Controller;
 
-@Data
+@Controller
 public class Java implements Course {
-
-//    private OfficeHours officeHours;
-    private ExtraSessions extraSessions;
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly Teaching Hours : " + (20 + extraSessions.getHours()));
+        System.out.println("Weekly Teaching Hours : ");
     }
 
 }
---
+++ spring/Spring-03-BEanScope-Singleton-Prototype/src/main/java/services/OfficeHours.java
@@ -0,0 +1,13 @@
+package services;
+
+import interfaces.ExtraSessions;
+
+public class OfficeHours implements ExtraSessions {
+
+    @Override
+    public int  getHours() {
+        return 5;
+    }
+
+
+}
