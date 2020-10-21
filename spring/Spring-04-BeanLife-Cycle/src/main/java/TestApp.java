4d990343994dcac1e775f3f86a84283ff465d608
LepakRuslan <Lepak1987@gmail.com>
2020-10-20 20:23:25 -0500

    Bean Life Cycle

--- spring/Spring-04-BeanLife-Cycle/src/main/java/TestApp.java
+++ spring/Spring-04-BeanLife-Cycle/src/main/java/TestApp.java
@@ -6,12 +6,11 @@ public class TestApp {
     public static void main(String[] args) {
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
 
-        Course course1 = container.getBean("java", Course.class);
-        Course course2 = container.getBean("java", Course.class);
+        Course course = container.getBean("java", Course.class);
 
-        System.out.println("Pointing to the same object: " + (course1 == course2));
-        System.out.println("Memory location for the course1: " + course1);
-        System.out.println("Memory location for the course2: " + course2);
+        course.getTeachingHours();
+
+        ((ClassPathXmlApplicationContext) container).close();
 
 
     }
