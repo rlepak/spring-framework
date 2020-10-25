aa880695ab446616bc8652b423e9ccf3bdaa1dcc
LepakRuslan <Lepak1987@gmail.com>
2020-10-25 16:50:57 -0500

    Java Code Configuration

--- spring/Spring-06-JavaCode-Configuration/src/main/java/TestApp.java
+++ spring/Spring-06-JavaCode-Configuration/src/main/java/TestApp.java
@@ -13,6 +13,8 @@ public class TestApp {
 
         Course course = container.getBean("java", Course.class);
 
+        Course course1 = container.getBean("java", Course.class);
+
         System.out.println(course.toString());
         System.out.println(course.toString());
 
