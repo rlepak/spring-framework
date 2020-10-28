fef951c78a01ff1b18584bba3c55f88d60b688a8
LepakRuslan <Lepak1987@gmail.com>
2020-10-27 20:10:29 -0500

    Spring Boot Demo

--- spring/Spring-07-SpringBoot-Demo/src/main/java/com/app/services/Java.java
+++ spring/Spring-07-SpringBoot-Demo/src/main/java/com/app/services/Java.java
@@ -2,6 +2,9 @@ package com.app.services;
 
 
 import com.app.interfaces.Course;
+import com.app.interfaces.ExtraSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -9,9 +12,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class Java implements Course {
 
+   private ExtraSession extraSession;
+
+   @Value("${instructor}")
+   private String instructor;
+
+   @Autowired
+    public Java(ExtraSession extraSession) {
+        this.extraSession = extraSession;
+    }
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Teaching hours 20");
+        System.out.println("Teaching hours " + (20 + extraSession.getHours()));
+        System.out.println(instructor);
     }
 }
