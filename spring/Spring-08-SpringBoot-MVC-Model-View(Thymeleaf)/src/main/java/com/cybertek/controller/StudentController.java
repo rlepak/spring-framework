769bac13a090e1a51f2660ccc21012abdf439388
LepakRuslan <Lepak1987@gmail.com>
2020-11-20 16:07:49 -0600

    MVC - Model

--- spring/Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
+++ spring/Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.cybertek.controller;
 
 
+import com.cybertek.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,15 @@ public class StudentController {
 
         List<Integer> numbers = Arrays.asList(4,6,3,6,8,8);
         model.addAttribute("numbers", numbers);
+
+        Student student = new Student("1", "Mike", "Smith");
+        model.addAttribute("student", student);
+
         return "student/welcome";
+    }
+
+    @GetMapping("/register")
+    public String homePage2(){
+        return "student/register";
     }
 }
---
+++ spring/Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/model/Student.java
@@ -0,0 +1,16 @@
+package com.cybertek.model;
+
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+
+    private String id;
+    private String firstName;
+    private String lastName;
+}
