418edf806d3c3669dac11f1d187eb56e08791462
LepakRuslan <Lepak1987@gmail.com>
2020-11-19 21:42:41 -0600

    MVC - Model

--- spring/Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ spring/Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -2,10 +2,7 @@ package com.cybertek.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -35,5 +32,25 @@ public class HomeController {
     public String getMappingEx2(){
         return "home";
     }
+
+    @GetMapping("/home/{name}/{email}")
+    public String getName(@PathVariable("name") String name, @PathVariable String email){
+        System.out.println("name is " + name);
+        System.out.println("email is " + email);
+        return "home";
+    }
+
+//    @GetMapping("/home/course")
+//    public String requestParamEx(@RequestParam("course") String course){
+//        System.out.println("name is " + course);
+//        return "home";
+//    }
+
+   @GetMapping("home/course")
+    public String requestParam2(@RequestParam(value = "course", required = false, defaultValue = "Cybertek") String name){
+       System.out.println("name is " + name);
+       return "home";
+   }
+
 
 }
---
+++ spring/Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.cybertek.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping("/welcome")
+    public String homePage(Model model) {
+        String date = String.valueOf(LocalDateTime.now());
+
+        model.addAttribute("name", "Ozzy");
+        model.addAttribute("date", date);
+
+        List<Integer> numbers = Arrays.asList(4,6,3,6,8,8);
+        model.addAttribute("numbers", numbers);
+        return "student/welcome";
+    }
+}
