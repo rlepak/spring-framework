10e3f617d238ecc4646e7bb95993368486d8dc07
LepakRuslan <Lepak1987@gmail.com>
2020-11-19 18:37:52 -0600

    MVC - Controller

--- spring/Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ spring/Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.cybertek.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,55 +10,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+
     @RequestMapping("/")
-    public String getRequestMapping(){
+    public String getRequestMapping() {
         return "home";
     }
 
-//    @RequestMapping("/home")
-    @RequestMapping(method = RequestMethod.GET, value = "/home")
-    public String getRequestMapping1(){
+    @RequestMapping( method = RequestMethod.GET, value = "/ozzy")
+    public String getRequestMapping2() {
         return "home";
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/home")
-    public String getRequestMapping2(){
+    @RequestMapping(method = RequestMethod.POST, value = "/cybertek")
+    public String getRequestMapping3() {
         return "home";
     }
 
     @GetMapping("/spring")
-    public String getMapping(){
+    public String getMappingEx(){
         return "home";
     }
 
     @PostMapping("/spring")
-    public String getMapping1(){
+    public String getMappingEx2(){
         return "home";
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
