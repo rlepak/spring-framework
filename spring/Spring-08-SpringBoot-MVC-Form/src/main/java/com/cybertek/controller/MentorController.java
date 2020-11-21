b06b552b4afc90c544d95495b5806aa422056bea
LepakRuslan <Lepak1987@gmail.com>
2020-11-20 19:14:57 -0600

    MVC - Form

---
+++ spring/Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -0,0 +1,27 @@
+package com.cybertek.controller;
+
+
+import com.cybertek.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        model.addAttribute("mentor", new Mentor());
+
+        List<String> batchList = Arrays.asList("B7", "B11", "B14", "B15", "B17", "B18", "B19");
+        model.addAttribute("batchList", batchList);
+
+        return "/mentor/mentor-register";
+    }
+}
--- spring/Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/model/Mentor.java
+++ spring/Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/model/Mentor.java
@@ -14,7 +14,7 @@ public class Mentor {
     private String lastName;
     private String email;
     private String gender;
-    private boolean gratuated;
+    private boolean graduated;
     private String batch;
     private String company;
 
