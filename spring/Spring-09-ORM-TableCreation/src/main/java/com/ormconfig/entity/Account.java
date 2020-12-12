43b3463b4bb7340df436d8f27e70f4351db1aecd
LepakRuslan <Lepak1987@gmail.com>
2020-12-12 13:46:28 -0600

    ORM Table creation

---
+++ spring/Spring-09-ORM-TableCreation/src/main/java/com/ormconfig/entity/Account.java
@@ -0,0 +1,16 @@
+package com.ormconfig.entity;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.math.BigDecimal;
+
+@MappedSuperclass
+public class Account {
+
+    @Id
+    private Long ai;
+    private String owner;
+    private BigDecimal balance;
+    private BigDecimal interestRate;
+
+}
--- spring/Spring-09-ORM-TableCreation/src/main/java/com/ormconfig/entity/Car.java
+++ spring/Spring-09-ORM-TableCreation/src/main/java/com/ormconfig/entity/Car.java
@@ -1,15 +1,27 @@
 package com.ormconfig.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "cars")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Car {
 
     @Id
-    private int id;
+    private long id;
 
     private String make;
     private String model;
 
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
 }
