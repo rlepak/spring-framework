376590d94bc56a9249ceb159f7af6e1d9bfdee7e
LepakRuslan <Lepak1987@gmail.com>
2020-12-22 20:05:53 -0600

    ORM Mapping DirivedQueries

--- spring/Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmplyeeRepository.java
+++ spring/Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmplyeeRepository.java
@@ -1,4 +1,42 @@
 package com.cybertek.repository;
 
-public interface EmplyeeRepository {
+import com.cybertek.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface EmplyeeRepository extends JpaRepository<Employee, Integer> {
+
+
+    List<Employee> findByEmail(String email);
+
+    //display all employees with first name and last name, also show all emplyees with a email address
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstName, String lastName, String email);
+
+    //display all employees that first name is not
+    List<Employee> findAllByFirstNameIsNot(String firstName);
+
+    //Display all employees where last name starts with ""
+    List<Employee> findByLastNameStartingWith(String pattern);
+
+    // display all employees with salary higher than ""
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    // display all employees with salary higher than ""
+    List<Employee> findBySalaryLessThanEqual(Integer salary);
+
+    //Display all employees that has been hired between
+    List<Employee> findAllByHireDateBetween(LocalDate startDate, LocalDate endDate);
+
+    // Display all employees where salaries greater and qual to '' in order
+    List<Employee> findAllBySalaryIsGreaterThanEqualOrderBySalaryDesc(Integer salary);
+
+    //display top unique 3 employees that is making less than ''
+    List<Employee> findDistinctTop3BySalaryLessThanEqual(Integer salary);
+
+    //display all employees that do not have email address
+    List<Employee> findByEmailIsNull();
 }
