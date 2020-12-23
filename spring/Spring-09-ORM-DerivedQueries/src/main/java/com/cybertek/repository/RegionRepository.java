ad171d6e24b65669dad486e043a9076cb52565a5
LepakRuslan <Lepak1987@gmail.com>
2020-12-22 18:59:21 -0600

    ORM Mapping DirivedQueries

--- spring/Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/RegionRepository.java
+++ spring/Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/RegionRepository.java
@@ -4,9 +4,21 @@ import com.cybertek.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Integer> {
 
+        //display all region in Canada
+    List<Region> findByCountry(String country);
 
+    //display all region in Canada, without duplicates
+    List<Region> findDistinctByCountry(String country);
+
+    //display all regions with country name includes 'United'
+    List<Region> findAllByCountryContains(String country);
+
+    //display all regions with country name include "United" in order
+    List<Region> findAllByCountryContainsOrderByCountry(String country);
 
 }
