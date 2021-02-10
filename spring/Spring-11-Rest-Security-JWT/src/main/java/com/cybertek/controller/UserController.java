233de499d4098be5855b13d273652e561d785130
LepakRuslan <Lepak1987@gmail.com>
2021-02-09 19:55:56 -0600

    Spring Security JWT

--- spring/Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
+++ spring/Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
@@ -5,6 +5,8 @@ import com.cybertek.entity.User;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequestMapping("/api/v1/users")
 public class UserController {
 
@@ -20,6 +23,8 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/read")
+//    @PreAuthorize(value = "hasAuthority('USER')")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
     public ResponseEntity<ResponseWrapper> readAll(){
         List<User> users = userService.getAll();
         return ResponseEntity.ok(new ResponseWrapper("Done", users));
